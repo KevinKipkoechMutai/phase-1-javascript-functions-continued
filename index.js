0d2face72eaaef2092d4092095d143cf353a37a2
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-08-22 05:20:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+//saturdayFun
+function saturdayFun(Saturday) {
+    if (Saturday == "bathe my dog") {
+        return "This Saturday, I want to bathe my dog!";
+    } else {
+        return "This Saturday, I want to roller-skate!";
+    }
+}
+
+//mondayWork
+function mondayWork(Monday) {
+    if (Monday == "work from home") {
+        return "This Monday, I will work from home."
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+//wrapAdjective
+function wrapAdjective(result = '*') {
+   let wraps = function(emphatic) {
+    if (result=='*') {
+        emphatic = "a hard worker";
+    } else if (result == "||") {
+        emphatic = "a dedicated programmer";
+    }
+    return `You are ${result}${emphatic}${result}!`;
+   }
+   return wraps;
+}
+
